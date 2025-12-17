badbea7de3c8b65e82cd2ba32d4c149ef2a56896
DonkeyOP <157317642+DonkeyOP@users.noreply.github.com>
2025-12-17 12:49:55 -0500

    Begun working on orbit functions

--- Gravity_Simulator1.cpp
+++ Gravity_Simulator1.cpp
@@ -24,11 +24,40 @@ double getMass() const{
     return mass;
 }
 
-double oribtal_gravity(){
-    double gravity = ((G * mass) / (pow(radius,2)));
+double getRadius()const{
+    return radius;
+}
+
+std::array<double,3> getPosition() const{
+    return position;
 }
 
 
 
-
 };
+
+int main(){   // Putting Simulator Part in Main for now
+int const numPlanets = 2;  // Hardcoding # of planet
+
+std::array<double,3> pos = {0, 0, 0};  // Planet 1
+std::array<double,3> v = {0,0,0};
+double m = pow(10,30)*2;
+double r = pow(10,5);
+
+std::array<double,3> pos1 = {pow(10,8)*7, 0, 0};  // Planet 2
+std::array<double,3> v1 = {5.4,-5.4,0};
+double m1 = pow(10,22);
+double r1 = pow(10,7);
+
+
+Planet Planet1 = Planet(pos,v,m,r);
+Planet Planet2 = Planet(pos1,v1,m1,r1);
+std::array <Planet, numPlanets> Moving_Bodies = {Planet1, Planet2};  // Intializing planets into an array.
+
+for(int i = 0; i < numPlanets; i++){
+
+}
+
+
+
+}
